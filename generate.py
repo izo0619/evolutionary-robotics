fe644168f69e07bcca39a3662bfaff0076a8d37d
izo0619 <isabelzhong30@gmail.com>
2022-12-19 12:33:27 -0800

    twenty five towers

--- generate.py
+++ generate.py
@@ -1,8 +1,26 @@
 import pyrosim.pyrosim as pyrosim
 
 # tell pyrosim the name of the file where information about the world you're about to create should be stored
-pyrosim.Start_SDF("box.sdf")
+pyrosim.Start_SDF("boxes.sdf")
 # stores a box with initial position x=0, y=0, z=0.5, and length, width and height all equal to 1 meter
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+length = 1
+width = 1
+height = 1
+x=0
+y=0
+z=0.5
+# pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length, width, height])
+# pyrosim.Send_Cube(name="Box", pos=[x+1,y,z+1] , size=[length, width, height])
+for x_pos in range (5):
+    for y_pos in range(5):
+        cm_height = 0
+        z=0.5
+        for i in range(10):
+            i_length = length*(0.9**(i+1))
+            i_width =  width*(0.9**(i+1))
+            i_height = height*(0.9**(i+1))
+            # z = cm_height + 0.5*i_height
+            pyrosim.Send_Cube(name="Box", pos=[x_pos,y_pos,z+i] , size=[i_length, i_width, i_height])
+            # cm_height += i_height
 # tells pyrosim to close the sdf file
 pyrosim.End()
--- simulate.py
+++ simulate.py
@@ -1,8 +1,12 @@
 import pybullet as p
+import pybullet_data
 import time
 
 physicsClient = p.connect(p.GUI)
-p.loadSDF("box.sdf")
+p.setAdditionalSearchPath(pybullet_data.getDataPath()) # find data that comes with pybullet
+p.setGravity(0,0,-9.8) # add gravitational force
+planeId = p.loadURDF("plane.urdf") # add floor plane
+p.loadSDF("boxes.sdf")
 for i in range(1000):
     p.stepSimulation()
     time.sleep(1/60)
