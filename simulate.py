d8d079742caa9d7f2d1f7c8035578930c8bce913
izo0619 <isabelzhong30@gmail.com>
2022-12-19 12:41:31 -0800

    created world

--- simulate.py
+++ simulate.py
@@ -6,7 +6,7 @@ physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath()) # find data that comes with pybullet
 p.setGravity(0,0,-9.8) # add gravitational force
 planeId = p.loadURDF("plane.urdf") # add floor plane
-p.loadSDF("boxes.sdf")
+p.loadSDF("world.sdf")
 for i in range(1000):
     p.stepSimulation()
     time.sleep(1/60)
